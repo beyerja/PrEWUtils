8bc07fae80de9199ec3d9893db0ab29e00a231a7
Jakob <jakob.beyer@desy.de>
2020-04-02 09:59:24 +0200

    Add naming convention for chiral xs coefficient
    - Add naming convention for the coefficient describing the total chiral 
    cross section.

--- source/src/Names/CoefNaming.cpp
+++ source/src/Names/CoefNaming.cpp
@@ -17,5 +17,17 @@ std::string CoefNaming::lumi_fraction_name (
 
 //------------------------------------------------------------------------------
 
+std::string CoefNaming::chi_xs_coef_name ( 
+  const std::string & distr_name,
+  const std::string & chiral_config
+) {
+  /** Naming convention for the coefficient that stores the total chiral cross
+      section of a distribution.
+  **/
+  return "ChiXS_" + distr_name + "_" + chiral_config;
+}
+
+//------------------------------------------------------------------------------
+
 } // Namespace Names
 } // Namespace PrEWUtils
