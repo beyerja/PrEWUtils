7f48baa47623ef932d9923a5733d4a64d106ccc1
Jakob Beyer <beyerja@gmx.de>
2020-06-24 18:32:14 +0200

    Add function to convert distribution to coefficient
    - Add function to DataHelp that converts a given predicted distribution 
    into a vector of doubles that can be used to build a coefficient.

--- source/src/DataHelp/PredDistrHelp.cpp
+++ source/src/DataHelp/PredDistrHelp.cpp
@@ -1,4 +1,8 @@
 #include <DataHelp/PredDistrHelp.h>
+#include <Names/CoefNaming.h>
+
+// Includes from PrEW
+#include <Data/CoefDistr.h>
 
 #include "spdlog/spdlog.h"
 
@@ -25,6 +29,36 @@ double PredDistrHelp::get_pred_sum (
   }
   
   return sum;
+}
+
+//------------------------------------------------------------------------------
+
+std::vector<double>
+PredDistrHelp::pred_to_coef(const PrEW::Data::PredDistr &pred,
+                            const std::string &type) {
+  /** Turn the given part of the distribution (type=signal/background/total) 
+      into a differential coefficient.
+   **/
+  if ((type != "signal") && (type != "background") && (type != "total")) {
+    throw std::invalid_argument(("Unknown type of distr coef requested: "
+                                + type).c_str());
+  }
+   
+  int n_bins = pred.m_bin_centers.size();
+   
+  std::vector<double> coefs (n_bins, 0);
+  
+  // Get the coefficient depending on the requested type
+  for (int b=0; b<n_bins; b++) {
+    if ( (type == "signal") || (type == "total") ) {
+      coefs[b] += pred.m_sig_distr[b];
+    }
+    if ( (type == "background") || (type == "total") ) {
+      coefs[b] += pred.m_bkg_distr[b];
+    }
+  }
+  
+  return coefs;
 }
 
 //------------------------------------------------------------------------------
