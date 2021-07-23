c2dcee1328772fe14d5ba723ba0abe7da126431c
beyerja <beyerja@gmx.de>
2021-07-23 18:38:18 +0200

    Adjust TGCInfo to new coefficients
    - Adjust TGCInfo to the new coefficients that are produced by 
    PrEWSampleProduction.

--- source/src/SetupHelp/TGCInfo.cpp
+++ source/src/SetupHelp/TGCInfo.cpp
@@ -1,6 +1,8 @@
 #include <SetupHelp/TGCInfo.h>
 
+#include <map>
 #include <string>
+#include <vector>
 
 #include "spdlog/spdlog.h"
 
@@ -8,11 +10,46 @@ namespace PrEWUtils {
 namespace SetupHelp {
 
 //------------------------------------------------------------------------------
+
+struct CoefNaming {
+  /** Small struct holding the names of the linear and quadratic coefs for a
+      given style (~ convention).
+   **/
+  std::vector<std::string> linear{};
+  std::vector<std::string> quadratic{};
+};
+
+const CoefNaming RK_coef_names{
+    // Coefficient Names used by Robert Karl
+    {"TGCA", "TGCB", "TGCC"}, // linear
+    {"TGCD", "TGCE", "TGCF",  // quadratic
+     "TGCG", "TGCH", "TGCI"}  // mixed
+};
+const CoefNaming JB_coef_names{
+    // Coefficient Names used by Jakob Beyer
+    {"TGC_k_g", "TGC_k_k", "TGC_k_l"},   // linear
+    {"TGC_k_g2", "TGC_k_k2", "TGC_k_l2", // quadratic
+     "TGC_k_gk", "TGC_k_gl", "TGC_k_kl"} // mixed
+};
+
+//------------------------------------------------------------------------------
 // Constructors
 
 TGCInfo::TGCInfo(const std::vector<std::string> &distrs,
-                 const std::string &mode)
-    : m_mode(mode), m_distrs(distrs) {
+                 const std::string &mode, const std::string &style)
+    : m_distrs(distrs) {
+  /** Constructor needs a vector with the affected distributions,
+      as well as:
+      > mode ... parametrisation mode
+        -> Options: 
+            - linear 
+            - quadratic
+      > style ... style of naming convention
+        -> Options:
+            - RK : style that Robert Karl used
+            - JB : style that Jakob Beyer used
+   **/
+
   // Create the parameters
   std::vector<std::string> par_names = {"Delta-g1Z", "Delta-kappa_gamma",
                                         "Delta-lambda_gamma"};
@@ -28,22 +65,34 @@ TGCInfo::TGCInfo(const std::vector<std::string> &distrs,
   std::string fct_name{};
   std::vector<std::string> coef_names{};
 
-  if (m_mode == "linear") {
-    fct_name = "Linear3DPolynomial_Coeff";
-    coef_names = {"One", "TGCA", "TGCB", "TGCC"};
-  } else if (m_mode == "quadratic") {
-    fct_name = "Quadratic3DPolynomial_Coeff";
-    coef_names = {
-        "One",  "TGCA", "TGCB", "TGCC", // linear
-        "TGCD", "TGCE", "TGCF",         // quadratic
-        "TGCG", "TGCH", "TGCI"          // mixed
-    };
+  // Figure out which TGC coefficient naming convention to use
+  CoefNaming coef_naming{};
+  if (style == "RK") {
+    coef_naming = RK_coef_names;
+  } else if (style == "JB") {
+    coef_naming = JB_coef_names;
   } else {
-    throw std::invalid_argument("TGCInfo: Unknown mode " + m_mode);
+    throw std::invalid_argument("TGCInfo: Unknown style " + mode);
   }
 
+  // Figure out the correct function name and collect the need coef names
+  coef_names = {"One"};
+  if (mode == "linear") {
+    fct_name = "Linear3DPolynomial_Coeff";
+    coef_names.insert(coef_names.end(), coef_naming.linear.begin(),
+                      coef_naming.linear.end());
+  } else if (mode == "quadratic") {
+    fct_name = "Quadratic3DPolynomial_Coeff";
+    coef_names.insert(coef_names.end(), coef_naming.linear.begin(),
+                      coef_naming.linear.end());
+    coef_names.insert(coef_names.end(), coef_naming.quadratic.begin(),
+                      coef_naming.quadratic.end());
+  } else {
+    throw std::invalid_argument("TGCInfo: Unknown mode " + mode);
+  }
+
+  // Add the prediction link (info set later)
   PrEW::Data::FctLink fct_link{fct_name, par_names, coef_names};
-  // Add the generic prediction link (info set later)
   m_pred_links.push_back(PrEW::Data::PredLink{{}, {fct_link}, {}});
 }
 
